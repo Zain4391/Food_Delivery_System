a7316fbb61a62980bf07eeebb562bd22bf17c54d
Zain4391 <manutdfast91@gmail.com>
2026-01-01 00:56:42 +0500

    Auth module done 2.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,21 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from '../users/entities/user.entity';
 import { DeliveryDriver } from '../drivers/entities/driver.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtCustomerStrategy } from './strategy/jwt-customer.strategy';
+import { DriverStrategy } from './strategy/jwt-driver.strategy';
+import { JwtCustomerGuard } from './guards/customer.guard';
+import { JwtDriverGuard } from './guards/driver.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer, DeliveryDriver])],
+  imports: [
+    TypeOrmModule.forFeature([Customer, DeliveryDriver]),
+    PassportModule,
+    JwtModule.register({})
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService]
+  providers: [AuthService, JwtCustomerStrategy, DriverStrategy, JwtCustomerGuard, JwtDriverGuard],
+  exports: [AuthService, JwtCustomerGuard, JwtDriverGuard]
 })
 export class AuthModule {}
