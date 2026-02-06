904e4ac25ab5befdc51336601b696a97ab70276f
Zain4391 <manutdfast91@gmail.com>
2026-02-07 00:53:28 +0500

    CORS fix done.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { getRabbitMQConfig, QUEUE_CONFIGS } from './rabbitmq/rabbitmq.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   app.connectMicroservice(getRabbitMQConfig(QUEUE_CONFIGS.ORDERS));
   app.connectMicroservice(getRabbitMQConfig(QUEUE_CONFIGS.RESTAURANTS));
