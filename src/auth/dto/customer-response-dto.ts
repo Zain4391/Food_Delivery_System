1784d9067591506811bc17b9dc1db2d5aed88820
Zain Rasool <zainrasoolhashmi@gmail.com>
2026-03-21 01:45:39 +0500

    fix: map profile_image_url (entity) -> profile_img_url (DTO) in constructors

--- src/auth/dto/customer-response-dto.ts
+++ src/auth/dto/customer-response-dto.ts
@@ -1,7 +1,6 @@
 import { Exclude, Expose } from "class-transformer";
 import { UserResponseDTO } from "./user-response-dto";
 
-
 export class CustomerResponseDTO extends UserResponseDTO {
 
     @Expose()
@@ -10,11 +9,24 @@ export class CustomerResponseDTO extends UserResponseDTO {
     @Expose()
     profile_img_url?: string;
 
+    @Expose()
+    role?: string;
+
+    @Expose()
+    created_at?: Date;
+
+    @Expose()
+    updated_at?: Date;
+
     @Exclude()
     password: string;
 
-    constructor(parttial: Partial<CustomerResponseDTO>) {
-        super(parttial)
-        Object.assign(this, parttial);
+    constructor(partial: Partial<CustomerResponseDTO> & { profile_image_url?: string }) {
+        super(partial);
+        Object.assign(this, partial);
+        // Entity stores 'profile_image_url'; DTO exposes it as 'profile_img_url'
+        if (partial.profile_image_url !== undefined) {
+            this.profile_img_url = partial.profile_image_url;
+        }
     }
 }
--- src/auth/dto/driver-response-dto.ts
+++ src/auth/dto/driver-response-dto.ts
@@ -2,8 +2,7 @@ import { Exclude, Expose } from "class-transformer";
 import { UserResponseDTO } from "./user-response-dto";
 import { VEHICLE_TYPE } from "src/drivers/entities/driver.entity";
 
-
-export class DriverResponseDTO extends UserResponseDTO{
+export class DriverResponseDTO extends UserResponseDTO {
 
     @Expose()
     phone: string;
@@ -17,11 +16,24 @@ export class DriverResponseDTO extends UserResponseDTO{
     @Expose()
     profile_img_url?: string;
 
+    @Expose()
+    role?: string;
+
+    @Expose()
+    created_at?: Date;
+
+    @Expose()
+    updated_at?: Date;
+
     @Exclude()
     password: string;
 
-    constructor(partial: Partial<DriverResponseDTO>) {
+    constructor(partial: Partial<DriverResponseDTO> & { profile_image_url?: string }) {
         super(partial);
         Object.assign(this, partial);
+        // Entity stores 'profile_image_url'; DTO exposes it as 'profile_img_url'
+        if (partial.profile_image_url !== undefined) {
+            this.profile_img_url = partial.profile_image_url;
+        }
     }
 }
